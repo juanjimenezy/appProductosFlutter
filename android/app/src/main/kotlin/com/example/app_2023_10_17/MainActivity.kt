7086877f00c31ed4c952a9e7c638fb2e4ebd12b4
Juan Jimenez <juanjiimenez8@gmail.com>
2023-11-07 22:55:28 -0500

    lista datos quemados

---
+++ android/app/src/main/kotlin/com/example/app_2023_10_17/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_2023_10_17
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'Principal.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "flutter demo",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      // home: const Inicio(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const Principal(),
+      },
+    );
+  }
+}
